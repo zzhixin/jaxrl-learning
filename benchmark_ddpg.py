b2a9d1ab64a413ed1cfcb03352f1c6c3a2b2be4f
zzhixin <zhixinzhao@foxmail.com>
2025-12-17 18:24:30 +0800

    more clear benchmark

--- benchmark_ddpg.py
+++ benchmark_ddpg.py
@@ -1,10 +1,17 @@
 import time
 import ddpg
+import warnings
+warnings.filterwarnings("ignore")
+import gymnax
 import jax
+jax.config.update("jax_platform_name", "cpu")
 from jax import random, numpy as jnp
+from flax import linen as nn
+import optax
+import time
+from replay_buffer import ReplayBuffer, ReplayBufferState
 from colorama import Fore, Style, init
 init(autoreset=True)
-
 
 
 config = {
@@ -41,27 +48,23 @@ def warmup_inner(train_one_step, rollout_and_push, update_model,
                  buffer, buffer_state,
                  num_steps_per_rollout,):
     eps_cur = jnp.float32(0.5)
-    combs = ((False, False), (False, True), (True, False), (True, True))
-    for is_update_model, is_update_target_model in combs:
 
-        train_key, loss, *train_state = train_one_step(
-            train_key, 
-            qnet, actor, opt, 
-            qnet_params, actor_params, target_qnet_params, target_actor_params,
-            qnet_opt_state, actor_opt_state, 
-            env, env_params, env_states, 
-            eps_cur, 
-            gamma, tau, 
-            is_update_model, is_update_target_model,
-            buffer, buffer_state,
-            num_steps_per_rollout,
-        )
-        qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state, env_states, buffer_state = train_state
+    train_key, loss, *train_state = train_one_step(
+        train_key, 
+        qnet, actor, opt, 
+        qnet_params, actor_params, target_qnet_params, target_actor_params,
+        qnet_opt_state, actor_opt_state, 
+        env, env_params, env_states, 
+        eps_cur, 
+        gamma, tau, 
+        buffer, buffer_state,
+        num_steps_per_rollout,
+    )
+    qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state, env_states, buffer_state = train_state
     print("warmup finished")
     print(f"{Fore.GREEN}rollout_and_push compile times: {rollout_and_push._cache_size()}")
     print(f"{Fore.GREEN}update_model compile times: {update_model._cache_size()}")
     return qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state, env_states, buffer_state
-
 
 def warmup_outer(train_one_step, rollout_and_push, update_model,
                  train_key, gamma, tau, 
@@ -72,25 +75,53 @@ def warmup_outer(train_one_step, rollout_and_push, update_model,
                  buffer, buffer_state,
                  num_steps_per_rollout,):
     eps_cur = jnp.float32(0.5)
-    combs = ((False, False), (False, True), (True, False), (True, True))
-    for is_update_model, is_update_target_model in combs:
-
-        train_key, loss, *train_state = train_one_step(
-            train_key, 
-            qnet, actor, opt, 
-            qnet_params, actor_params, target_qnet_params, target_actor_params,
-            qnet_opt_state, actor_opt_state, 
-            env, env_params, env_states, 
-            eps_cur, 
-            gamma, tau, 
-            is_update_model, is_update_target_model,
-            buffer, buffer_state,
-            num_steps_per_rollout,
-        )
-        qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state, env_states, buffer_state = train_state
+    train_key, loss, *train_state = train_one_step(
+        train_key, 
+        qnet, actor, opt, 
+        qnet_params, actor_params, target_qnet_params, target_actor_params,
+        qnet_opt_state, actor_opt_state, 
+        env, env_params, env_states, 
+        eps_cur, 
+        gamma, tau, 
+        buffer, buffer_state,
+        num_steps_per_rollout,
+    )
+    qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state, env_states, buffer_state = train_state
     print("warmup finished")
     print(f"{Fore.GREEN}train_one_step compile times: {train_one_step._cache_size()}")
     return qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state, env_states, buffer_state
+
+
+def train_one_step(
+        key, 
+        qnet: nn.Module, actor: nn.Module, opt: optax.GradientTransformation, 
+        qnet_params, actor_params, target_qnet_params, target_actor_params,
+        qnet_opt_state: optax.OptState, actor_opt_state: optax.OptState, 
+        env, env_params: gymnax.EnvParams, env_state: gymnax.EnvState, 
+        eps_cur: jax.Array,
+        gamma: float, tau: float, 
+        buffer: ReplayBuffer, buffer_state: ReplayBufferState,
+        num_steps: int):
+    
+    key, rollout_key, sample_key = jax.random.split(key, 3)
+
+    env_state, buffer_state = ddpg.rollout_and_push(rollout_key, 
+                     actor, actor_params,
+                     env, env_params, env_state, 
+                     eps_cur,
+                     buffer, buffer_state,
+                     num_steps)
+
+    loss = jnp.array(0.0)
+    sampled_experiences = buffer.sample(sample_key, buffer_state)
+    loss, qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state = \
+        ddpg.update_model(sampled_experiences,
+                    qnet, actor, opt, 
+                    qnet_params, actor_params, target_qnet_params, target_actor_params,
+                    qnet_opt_state, actor_opt_state, 
+                    gamma
+                    )
+    return  key, loss, qnet_params, target_qnet_params, qnet_opt_state, actor_params, target_actor_params, actor_opt_state, env_state, buffer_state
 
 
 def benchmark(config, warmup, n_iters=1000):
@@ -115,7 +146,7 @@ def benchmark(config, warmup, n_iters=1000):
         qnet_params, target_qnet_params, qnet_opt_state, 
         actor_params, target_actor_params, actor_opt_state, 
         env_states, buffer_state
-    ) = warmup(ddpg.train_one_step, ddpg.rollout_and_push, ddpg.update_model,
+    ) = warmup(train_one_step, ddpg.rollout_and_push, ddpg.update_model,
                 train_key, gamma, tau, 
                 qnet, actor, opt, 
                 qnet_params, actor_params, target_qnet_params, target_actor_params,
@@ -130,7 +161,7 @@ def benchmark(config, warmup, n_iters=1000):
 
     for iter in range(n_iters):
         eps_cur = jnp.float32(0.5)
-        train_key, loss, *train_state = ddpg.train_one_step(
+        train_key, loss, *train_state = train_one_step(
             train_key, 
             qnet, actor, opt, 
             qnet_params, actor_params, target_qnet_params, target_actor_params,
@@ -138,7 +169,6 @@ def benchmark(config, warmup, n_iters=1000):
             env, env_params, env_states, 
             eps_cur, 
             gamma, tau, 
-            True, True,
             buffer, buffer_state,
             num_steps_per_rollout,
         )
@@ -161,14 +191,13 @@ if __name__ == "__main__":
 
     # Jit the outmost function train_one_step
     print("\n-------- Jit the outmost function train_one_step ---------")
-    ddpg.train_one_step = jax.jit(
-        ddpg.train_one_step,
+    train_one_step = jax.jit(
+        train_one_step,
         static_argnames=["qnet", "actor", "opt", "env", 
-                         "buffer", "num_steps",
-                         "is_update_target_model", "is_update_model"],
+                         "buffer", "num_steps"],
         donate_argnames=["buffer_state"])
     benchmark(config, warmup=warmup_outer)
-    print(f"{Fore.GREEN}train_one_step compile times: {ddpg.train_one_step._cache_size()}")
+    print(f"{Fore.GREEN}train_one_step compile times: {train_one_step._cache_size()}")
 
 
     # # Only jit inner function
